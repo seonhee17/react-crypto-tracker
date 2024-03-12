a68991477b02a5a40525678fdb04d4453a73887f
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-12 23:08:33 +0900

    #6.0 DartMode part One

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { theme } from "./theme";
 /* import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; */
 import { QueryClient,QueryClientProvider } from "react-query";
 // devtools를 깔면 다시 react-query를 쓸수 있다.
@@ -17,9 +15,7 @@ root.render(
   <React.StrictMode>
     {/* //설치 */}
     <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider> 
     </QueryClientProvider>
   </React.StrictMode>,
 );
