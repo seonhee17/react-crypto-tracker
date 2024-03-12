7c497800347193ea13e956ddf96c337607cf0151
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-12 11:15:38 +0900

    5.4 coin List  꾸미고
        - Link 사용법 path , state
        - Link에서 넘겨받은  state 사용법
        - Location 사용

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,80 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { styled } from "styled-components";
+import { useLocation } from "react-router-dom";
 
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items : center;
+`;
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
+`;
+
+const Title = styled.h1`
+font-size: 48px;
+color : ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+`;
+
+interface RouteParams {
+    coinId : string;
+} 
+interface RouteState {
+    name : string;
+} 
+
+/* react-router-dom v6 부터 제네릭을 지원하지 않습니다.
+
+interface RouterState {
+name: string;
+}
+
+const location = useLocation();
+const name = location.state as RouterState; */
 
 function Coin(){
     
-    const { coinId } = useParams();
-    
+    const [isLoading , setLoading] = useState(true);
+    const [coin , setCoin] = useState([]);
+    /* 
+    const { coinId } = useParams(); */
+    const location = useLocation();
+    const state = location.state as RouteState;
+    console.log(location);
+   
+    //component가 생성될 때 한번만 코드를 실행하려면 useEffect 사용
+
+ 
+
+
+
+
+    //state : 
+    // coins 화면을 통하지 않으면 에러가 남.
+    // 그래서 state 뒤에 ?를 붙임.
+    // state가 존재하면 , state.name을 가져오고 아니면 loading..을 띄운다.
+ 
     return (
-        <h1>Coin's  key is : {coinId}</h1>
+        <Container>
+        <Header>
+            <Title> { state?.name || "Loading.." }</Title>
+        </Header>
+        { isLoading ? 
+            (<Loader>is Loading...</Loader> )
+            : (
+                    null           
+            )
+        }
+        </Container>  
     );
 }
 
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const Container = styled.div`
@@ -21,10 +21,12 @@ const Coin = styled.li`
     color: ${(props) => props.theme.bgColor};
     border-radius: 15px;
     margin-bottom: 10px;
+   
     a{
+        display: flex;
+        align-items: center;
         padding: 20px;
         transition : color 0.2s ease-in ;
-        display: block;
     }
     &:hover {
         a{
@@ -51,6 +53,14 @@ const Loader = styled.span`
     text-align: center;
 `;
 
+const Img = styled.img`
+width :35px;
+height: 35px;
+margin-right: 10px;
+`;
+
+
+
 function Coins(){
     const [coins ,setCoins] = useState<CoinInterface[]>([]);
     const [isLoading , setLoading] = useState(true);
@@ -65,7 +75,11 @@ function Coins(){
         })();
     },[]);
 
+    //Link로 연결된 곳의 페이지에서  state로 가져다 쓸수 있ㄷ.
+    //Coin.tsx 페이지서 가져다 쓰기
+    let {state} = useLocation();
 
+    //state 비하인드 더씬 방식  parameter를 사용해서 다른화면으로 
     return (
         <Container>
         <Header>
@@ -74,10 +88,28 @@ function Coins(){
         { isLoading ? 
             (<Loader>is Loading...</Loader> )
             :   
-            (        <CoinList>
+            (<CoinList>
                 {coins.map((coin) => (
                     <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                       {/*  <Link to={`/${coin.id}`} >
+                            <Img src={`https://cryptoicon-api.pages.dev/api/icon/${coin.symbol.toLowerCase()}`}/>    
+                            {coin.name} &rarr;</Link> */}
+                        <Link    to={{ pathname :`/${coin.id}`, }} 
+                              state={{  name : coin.name}} >
+                            <Img src={`https://cryptoicon-api.pages.dev/api/icon/${coin.symbol.toLowerCase()}`}/>    
+                            {coin.name} &rarr;</Link>
+                         {/*
+                         <Link to="/course?sort=name"
+                        Link 사용법
+                        <Link  to={{
+                             pathname:"/course",
+                             search : "?sort=ame",
+                             hash : "#the-hash",
+                             }}
+                             state = {{ fromDashboard : true }}   
+                        /> */}
+
+
                     </Coin>
                 ))}
             
